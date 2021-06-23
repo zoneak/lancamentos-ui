af81edb5ad447836e4b92e15f38200b73c5394bb
Adriano Kaito <adriano.kaito@gmail.com>
2021-06-23 12:28:04 -0300

    arquivo server.js para testes com express da pasta dist em dev

---
+++ server.js
@@ -0,0 +1,26 @@
+/*
+ * Para testar com o express, primeiro alterar o environment.prod.ts para urls locais conforme abaixo:
+ *
+ *  export const environment = {
+ *   production: true,
+ *   apiUrl: 'http://localhost:8082',
+ *   allowedDomains: [ /localhost:8082/ ],
+ *   disallowedRoutes: [/\/oauth\/token/]
+ *  };
+ *
+ * Gerar um build pelo ng build
+ * Em seguida, rodar este arquivo no prompt pelo comando: node server.js
+ * Assim estaremos acessando os arquivos gerados no build para testarmos
+ * Não esquecer de retornar as configs do environment.prod para os endereços de prod e gerar o build novamente após os testes
+*/
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/lancamentos-ui'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/lancamentos-ui/index.html');
+});
+
+app.listen(4200);
