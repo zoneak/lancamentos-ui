68e71196d50f1385f6274a9f5c88745005a92f7c
Adriano Kaito <adriano.kaito@gmail.com>
2021-06-23 10:44:13 -0300

    tratativa de refresh token expirado

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { NotAuthenticatedError } from './../seguranca/money-http-interceptor';
 import { MessageService } from 'primeng/api';
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -7,13 +9,19 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class ErrorHandlerService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private router: Router
+  ) { }
 
   handle(errorResponse: any) {
     let msg: string;
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
     } else if (errorResponse instanceof HttpErrorResponse && errorResponse.status >= 400 && errorResponse.status <= 499) {
       msg = 'Ocorreu um erro ao processar a sua solicitação';
 
